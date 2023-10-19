64b07371880e2fdc6ed7aa136c807211f6622832
Swati Singh <swatisingh4575@gmail.com>
2023-10-20 00:36:50 +0530

    User profile hidden

--- src/app/user-info/user-info.component.ts
+++ src/app/user-info/user-info.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { UserService } from '../Service/user.service';
+//import { ActivatedRoute, Route } from '@angular/router';
+ import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-info',
@@ -8,16 +10,29 @@ import { UserService } from '../Service/user.service';
 })
 export class UserInfoComponent {
 
+  condition: boolean = true;
   respData: string|undefined ;
+  // currentRouter: ActivatedRoute | null | undefined;
   
-  constructor( private userService : UserService) {}
+  constructor( private userService : UserService, private route: Router, private currentRouter: ActivatedRoute) {}
   
   ngOnInit(): void {
     
+    if (!localStorage.getItem('token')) {
+      this.condition=false
+      console.log("oninit")
+       this.route.navigate(['../register'], {relativeTo: this.currentRouter});  
+      }
+   
+   
+   
+    console.log(localStorage.getItem('token'))
+
     this.userService.userProfile().subscribe((respData : any)=>{
       this.respData = respData;
     });
-
+    // localStorage.removeItem('token'); // token checked
+ 
   }
 
 }
